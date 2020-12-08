0e993447268bff13b32ec3d77c2e14f80512144c
shumbaashley <ashleytshumba@gmail.com>
2020-12-08 19:37:32 +0200

    added login, register and dashboard screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Fragment } from "react";
-
+import Home from "./components/screens/Home";
+import Login from "./components/screens/Login";
+import Register from "./components/screens/Register";
+import Tables from "./components/screens/Tables";
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Fragment>
-      Hello Admin
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <Route exact path="/tables" component={Tables}/>
+      </Switch>
+    </Router>
   );
 }
 
